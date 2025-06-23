017131952130457942576b28c096aed2afde9aea
nistala <snistala@miraclesoft.com>
2025-06-23 23:08:30 +0530

    Final change in ui

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -133,17 +133,8 @@ const SidebarProvider = React.forwardRef<
       <SidebarContext.Provider value={contextValue}>
         <TooltipProvider delayDuration={0}>
           <div
-            style={
-              {
-                "--sidebar-width": SIDEBAR_WIDTH,
-                "--sidebar-width-icon": SIDEBAR_WIDTH_ICON,
-                ...style,
-              } as React.CSSProperties
-            }
-            className={cn(
-              "group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar",
-              className
-            )}
+            
+           
             ref={ref}
             {...props}
           >
